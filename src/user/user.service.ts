134d23f9be68b9c3e27a541ad5058404c5d94ca2
Zakaria-zaki <03zaki95@gmail.com>
2021-08-01 16:10:38 +0200

    Ajout des données manquées

--- src/user/user.service.ts
+++ src/user/user.service.ts
@@ -1,16 +1,30 @@
 import { Injectable } from '@nestjs/common';
 import { CreateUserDto } from './dto/create-user.dto';
 import { UpdateUserDto } from './dto/update-user.dto';
+import { User } from './entities/user.entity';
 
 @Injectable()
 export class UserService {
+
+  private readonly users: User[] = [];
+
   create(createUserDto: CreateUserDto) {
     return 'This action adds a new user';
   }
 
-  findAll() {
-    return `This action returns all user`;
+  findAll(): User[] {
+    return this.users
+    
   }
+  constructor() {
+    this.users.push({
+      id: 1,
+      name: 'Zak',
+      email: 'zak@gmail.com',
+      password: 'zak1234',
+    });
+  }
+
 
   findOne(id: number) {
     return `This action returns a #${id} user`;
@@ -23,4 +37,6 @@ export class UserService {
   remove(id: number) {
     return `This action removes a #${id} user`;
   }
+
+  
 }
